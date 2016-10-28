4e1729e881ec5e233b87f0cfc537828d87c84eed
Lu <wlu000@CACSVML-14677.local>
2016-10-28 12:32:19 -0700

    move to config

--- config/config.js
+++ config/config.js
@@ -4,7 +4,11 @@ var config = {
     "logLevel": "trace",
     "index": "xj",
     "type": "event"
-	}
+	},
+  "spark": {
+    "command": "/Users/wlu000/spark/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class com.r2.xj.DataForecast /Users/wlu000/spark/spark-xj/target/xj-data-forecast-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+    //"location": "/home/ubuntu/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class com.r2.xj.DataForecast /home/ubuntu/spark-xj/target/xj-data-forecast-0.0.1-SNAPSHOT-jar-with-dependencies.jar"
+  }
 };
 
 module.exports = config;
--- routes/data.js
+++ routes/data.js
@@ -9,7 +9,7 @@ var data = {
 
   forecast: function(req, res) {
     console.log(req.query);
-    var command = '/Users/wlu000/spark/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class com.r2.xj.DataForecast /Users/wlu000/spark/spark-xj/target/xj-data-forecast-0.0.1-SNAPSHOT-jar-with-dependencies.jar';
+    var command = config.spark.command;
     command += ' ' + req.query.tag;
     exec(command, function(error, stdout, stderr) {
         console.log('stdout: ' + stdout);
